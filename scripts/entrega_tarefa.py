d3000e90344d6100f905cfe71126657b7427d4c3
pedro_akasaka <phakasaka@gmail.com>
2024-06-04 20:38:24 -0300

    Update entrega_tarefa.py

--- scripts/entrega_tarefa.py
+++ scripts/entrega_tarefa.py
@@ -2,16 +2,16 @@ import pyautogui as pa
 import time
 import os
 
-pa.PAUSE = 1
+pa.PAUSE = 2
 
-edge = 'C:/Users/0220482413046/Documents/pedro/macro/automacao/img/teams.png'
+edge = 'edge.png'
 
 local_edge = pa.locateCenterOnScreen(edge, confidence=0.8)
 
 pa.click(local_edge.x, local_edge.y)
 
 
-teams = 'C:/Users/0220482413046/Documents/pedro/macro/automacao/img/edge.png'
+teams = 'teams.png'
 
 local_teams = pa.locateCenterOnScreen(teams, confidence=0.8)
 
