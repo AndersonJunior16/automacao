62e9a7d3ee57d0dfa7f82864af05e31d8297aaeb
pedro_akasaka <phakasaka@gmail.com>
2024-06-04 21:23:13 -0300

    fim

--- scripts/entrega_tarefa.py
+++ scripts/entrega_tarefa.py
@@ -4,15 +4,17 @@ import os
 
 pa.PAUSE = 1
 
-edge = 'C:/Users/0220482413046/Documents/pedro/macro/automacao/img/teams.png'
+edge = '../img/teams.png'
 
 local_edge = pa.locateCenterOnScreen(edge, confidence=0.8)
 
 pa.click(local_edge.x, local_edge.y)
 
 
-teams = 'C:/Users/0220482413046/Documents/pedro/macro/automacao/img/edge.png'
+teams = '../img/edge.png'
 
 local_teams = pa.locateCenterOnScreen(teams, confidence=0.8)
 
 pa.click(local_teams.x, local_teams.y)
+
+
