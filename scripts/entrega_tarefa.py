60a04dc3ef3221a9c30c2d357df318f51c7bffe7
Breno Pavanelli <zayocomercial@gmail.com>
2024-06-04 21:22:28 -0300

    teste

--- scripts/entrega_tarefa.py
+++ scripts/entrega_tarefa.py
@@ -1,18 +1,13 @@
+import zipfile
 import pyautogui as pa
 import time
 import os
 
 pa.PAUSE = 2
 
-edge = 'edge.png'
+caminho_para_edge = 'edge.png'
 
-local_edge = pa.locateCenterOnScreen(edge, confidence=0.8)
+local_edge = pa.locateCenterOnScreen(caminho_para_edge, confidence=0.8)
 
 pa.click(local_edge.x, local_edge.y)
-
-
-teams = 'teams.png'
-
-local_teams = pa.locateCenterOnScreen(teams, confidence=0.8)
-
-pa.click(local_teams.x, local_teams.y)
+zipfile
