8acac5d6781aabe0902e4763bb17332197976fe2
Anderson Junior <juninho16carvalho@gmail.com>
2024-06-04 21:23:16 -0300

    1

--- scripts/entrega_tarefa.py
+++ scripts/entrega_tarefa.py
@@ -2,19 +2,24 @@ import pyautogui as pa
 import time
 import os
 
+# Set a pause between actions to avoid overwhelming the system
 pa.PAUSE = 2
 
+# Function to locate and click an image
+def locate_and_click(image_path, confidence=0.8):
+    location = pa.locateCenterOnScreen(image_path, confidence=confidence)
+    if location:
+        pa.click(location.x, location.y)
+        print(f'Clicked on {image_path} at ({location.x}, {location.y})')
+    else:
+        print(f'Could not find {image_path} on the screen')
+
+# Path to the images
 edge = 'edge.png'
-
-local_edge = pa.locateCenterOnScreen(edge, confidence=0.8)
-
-pa.click(local_edge.x, local_edge.y)
-
-
 teams = 'teams.png'
 
-local_teams = pa.locateCenterOnScreen(teams, confidence=0.8)
+# Click on Edge icon
+locate_and_click(edge)
 
-pa.click(local_teams.x, local_teams.y)
-
-hahahaha
+# Click on Teams icon
+locate_and_click(teams)
